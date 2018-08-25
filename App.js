57a9591072be6472ba61a7246b143d74387a63fc
Andreas Gropp <starmaerker@googlemail.com>
2018-08-25 07:58:04 +0200

    added AddEntry.js

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome Home Master</Text>
-        <Text>Lets start rolling.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <AddEntry />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6666ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
