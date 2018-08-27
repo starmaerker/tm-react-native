96f5bacbe7ff4f132af6c096e730eed3a153471a
Andreas Gropp <starmaerker@googlemail.com>
2018-08-27 11:32:56 +0200

    updated UdacitySlider

--- components/UdacitySlider.js
+++ components/UdacitySlider.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Slider, Text } from 'react-native'
 
-export default function UdacitySlider() {
+export default function UdacitySlider({ max, unit, step, value, onChange }) {
     return (
         <View>
-            <Text>UdacitySlider</Text>
+            <Slider 
+                step={step}
+                value={value}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
         </View>
     )
 }
