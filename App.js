351d18474725d5764e9c2154ec04d5ca998c77be
Andreas Gropp <starmaerker@googlemail.com>
2018-08-29 13:55:17 +0200

    created a store

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Text, View, StyleSheet, Slider } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import AddEntry from './components/AddEntry';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   state = {
@@ -8,19 +11,11 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <AddEntry />
-        <Slider
-          minimumValue={-10}
-          maximumValue={10}
-          step={1}
-          value={this.state.value}
-          onValueChange={(value) => this.setState(() => ({ value }))}
-        />
-        <Text>
-          Value : {this.state.value}
-        </Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
