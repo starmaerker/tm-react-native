c518bc55b24ab0fb9d6b03162262e52b7e67f6ae
Andreas Gropp <starmaerker@googlemail.com>
2018-08-29 06:28:59 +0200

    added api.js

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdacitySteppers from './UdacitySteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn({ onPress }) {
     return (
@@ -65,10 +66,14 @@ export default class AddEntry extends Component {
             sleep: 0,
             eat: 0
         }))
+
+        submitEntry({ key, entry })
     }
 
     reset = () => {
         const key = timeToString
+
+        removeEntry(key)
     }
 
     render() {
