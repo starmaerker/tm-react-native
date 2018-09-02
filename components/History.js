3183c509e853315bce24c41f6ddd7c54489ba7fe
Andreas Gropp <starmaerker@googlemail.com>
2018-09-02 11:18:01 +0200

    added MetricCard

--- components/History.js
+++ components/History.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
 
 class History extends Component {
     componentDidMount() {
@@ -21,17 +24,27 @@ class History extends Component {
             })
     }
     renderItem = ({ today, ...metrics }, formattedDay, key) => (
-        <View>
+        <View style={styles.item}>
             {today
-                ? <Text>{JSON.stringify(today)}</Text>
-                : <Text>{JSON.stringify(metrics)}</Text>
+                ? <View>
+                    <DateHeader date={formattedDay} />
+                    <Text style={stlyes.noDataText}>
+                        {today}
+                    </Text>
+                </View>
+                : <TouchableOpacity onPress={() => console.log('Pressed')}>
+                    <MetricCard metrics={metrics} date={formattedDay} />
+                </TouchableOpacity>
             }
         </View>
     )
     renderEmptyDate(formattedDay) {
         return (
-            <View>
-                <Text>No data for this day</Text>
+            <View style={styles.item}>
+                <DateHeader date={formattedDay} />
+                <Text style={styles.noDataText}>
+                    You didn't log any data on this day.
+                </Text>
             </View>
         )
     }
@@ -48,6 +61,30 @@ class History extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+})
 
 function mapStateToProps(entries) {
     return {
