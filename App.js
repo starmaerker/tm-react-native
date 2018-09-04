fb8ddd7d1e69cb1ac6a45dbaa6570eb6ef6386a6
Andreas Gropp <starmaerker@googlemail.com>
2018-09-04 06:25:17 +0200

    replaced tabnavigator with createMaterialTopTabNavigator

--- App.js
+++ App.js
@@ -1,44 +1,49 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Platform } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { createMaterialTopTabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation'
-import { lightPurp } from './utils/colors';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-
-function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>HOME</Text>
-    </View>
-  )
-}
-
-function Dashboard() {
-  return (
-    <View style={styles.container}>
-      <Text>Dashboard</Text>
-    </View>
-  )
-}
+import { createMaterialTopTabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 const Tabs = createMaterialTopTabNavigator({
-  Home: {
-    screen: Home,
+  History: {
+    screen: History,
     navigationOptions: {
-      tabBarIcon: () => <FontAwesome name='home' size={30} color='black' />
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
     }
   },
-  Dashboard: {
-    screen: Dashboard,
+  AddEntry: {
+    screen: AddEntry,
     navigationOptions: {
-      tabBarIcon: () => <FontAwesome name='dashboard' size={30} color='black' />
+      tabBarLabel: 'AddEntry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
     }
   }
-})
+}, {
+    navigationOptions: {
+      header: null
+    },
+
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  })
 
 export default class App extends React.Component {
   state = {
@@ -46,21 +51,13 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <Provider store={createStore(reducer)}>        
-        <View style={{flex: 1}}>
-          <View style={{height: 20}} />
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }} />
           <Tabs />
-          <History />
         </View>
       </Provider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: lightPurp,
-  }
-})
 
